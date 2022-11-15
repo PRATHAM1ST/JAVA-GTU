4f8c28512818070956a1d9bd1e548ce084c5cb45
Pratham <chudasamapratham142@gmail.com>
2022-11-15 23:35:15 +0530

    some more ques

--- MSE-2/dump.java
+++ MSE-2/dump.java
@@ -1,5 +1,6 @@
 abstract class Vegetable {
     String color;
+
     abstract void display();
 }
 
@@ -33,3 +34,116 @@ class Tomato extends Vegetable {
     }
 }
 
+interface A {
+    int a = 10;
+}
+
+interface C extends A {
+    int b = 20;
+}
+
+interface B extends A {
+    int c = 30;
+}
+
+class D implements C, B {
+    D() {
+        new SOP(a + b + c);
+    }
+}
+
+class NegativeException extends Exception {
+    NegativeException(String pos) {
+        new SOP("Its negative at pos " + pos);
+    }
+}
+
+int a[] = new int[] { 1, 2, 3, -1, 90 };try
+{
+    for (int i : a) {
+        if (i < 0) {
+            throw new NegativeException("" + i);
+        }
+    }
+}
+catch(
+    NegativeException e){
+    new SOP(e);
+}
+
+
+class InvalidStatus extends Exception{
+    InvalidStatus(){
+        new SOP("Shiv shiv shiv");
+    }
+}
+
+class ResourcesStatus{
+    int FREE = 0, OCCUPIED = 1, INACCESSIBLE = 2;
+    int statusRef[][] = new int[3][3];
+
+    ResourcesStatus(int statusArr[][]){
+        statusRef = statusArr;
+    }
+
+    void processStausCount(){
+        int freeCounter = 0, occupiedCounter = 0, inaccessibleCouner = 0;
+        for(int i[]: statusRef){
+            for(int j: i){
+                if(j == FREE){
+                    freeCounter++;
+                }
+                else if(j == OCCUPIED){
+                    occupiedCounter++;
+                }
+                else{
+                    inaccessibleCouner++;
+                }
+            }
+        } 
+        new SOP("free: " + freeCounter + " occupied: " + occupiedCounter + " inaccessible: " + inaccessibleCouner);
+        exceptionChecker(freeCounter, occupiedCounter);
+    }
+
+    static void exceptionChecker(int freeCounter, int occupiedCounter){
+        try{
+            if(freeCounter < occupiedCounter){
+                throw new InvalidStatus();
+            }
+        }
+        catch(InvalidStatus i){
+            new SOP("I didn't thought of this");
+        }
+    }
+
+}
+
+int a[][] = new int[3][];
+a[0] = new int[]{1, 2, 2};
+a[1] = new int[]{1, 2, 1};
+a[2] = new int[]{0, 2, 0};
+ResourcesStatus r = new ResourcesStatus(a);
+r.processStausCount();
+
+
+class RangeException extends Exception{
+    RangeException(){
+        new SOP("Given value is not in the range.\nhence is not acceptible");
+    }
+}
+
+
+int marks[] = new int[]{10, 2, -1, 30, 4};
+try{
+    for(int i : marks){
+        if(0 <= i && i <= 20){
+            new SOP(i);
+        }
+        else{
+            throw new RangeException();
+        }
+    }
+}
+catch(RangeException r){
+    new SOP(r);        
+}
--- MSE-2/exam2.java
+++ MSE-2/exam2.java
@@ -1,25 +1,28 @@
 import cool.SOP;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.io.ObjectInputStream.GetField;
 
-interface A{
-    int a = 10;
-}
-
-interface C extends A{
-    int b = 20;
-}
-
-interface B extends A{
-    int c = 30;
-}
-
-class D implements C, B{
-    D(){
-        new SOP(a + b + c);
+class UserThread extends Thread{
+    public void run(){
+        new SOP(getPriority() + " " + getName() + " " + getId() + " " + getState() + " " + getClass() + " " + isAlive() + " " + isDaemon());
     }
 }
 
+
 public class exam2 {
-    public static void main(String args[]) {
-        new D();
+    public static void main(String[] args){
+        Thread t1 = new UserThread();
+        Thread t2 = new UserThread();
+
+        t1.setPriority(1);
+        t2.setPriority(2);
+
+        t1.setName("t1");
+        t2.setName("t2");
+
+        t1.start();
+        t2.start();
     }
 }
